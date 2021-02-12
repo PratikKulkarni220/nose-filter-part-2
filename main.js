216646a1d2488aad192e467129d835c57aec098f
PratikKulkarni220 <73877259+PratikKulkarni220@users.noreply.github.com>
2021-02-12 22:04:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(450, 450);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(450, 450);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 450, 450);
+}
